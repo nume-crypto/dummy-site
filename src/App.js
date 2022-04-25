68a5d8b682a8e758ae539f6a6b12b8f3ba00054e
Vikram <vikram710v@gmail.com>
2022-04-25 15:44:27 +0530

    Feat: Integrate with npm package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const checkoutWithNume = require('nume-pay');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const handleSubmit = async () => {
+		const payload = {
+			merchantId: 4,
+			referenceId: 'ref',
+			amountUsd: 20.5,
+			products: [
+				{
+					skuId: 'he',
+					count: 4,
+				},
+			],
+		};
+		const god = await checkoutWithNume(payload);
+		console.log(god);
+	};
+	return (
+		<div className="App">
+			<button onClick={handleSubmit}> Pay with nume</button>
+		</div>
+	);
+};
 
 export default App;

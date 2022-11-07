e53cf2c663dd0e867a6ad7cf96c864c543172729
Vikram <vikram710v@gmail.com>
2022-11-07 14:10:30 +0530

    Fix: Change local api url

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "change to local";
 cp ./node_modules/nume-pay/index.js ./node_modules/nume-pay/index.js.backup
-echo "const API_URL = 'https://dev.api.numecrypto.com/api/';
+echo "const API_URL = 'http://localhost:3000/api/';
 const CHECKOUT_URL = 'http://localhost:5002/';" >./node_modules/nume-pay/index.js
 cat ./node_modules/nume-pay/index.js.backup | tail -n+3 >>./node_modules/nume-pay/index.js
 rm ./node_modules/nume-pay/index.js.backup
